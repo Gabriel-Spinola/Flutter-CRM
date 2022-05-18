695a4cb953531420d5d21296a616aeeefc0c35b4
gseven-gdv <zerogravitystd@gmail.com>
2022-05-18 14:23:44 -0300

    -m

--- crm_app/lib/Models/model_abstract.dart
+++
@@ -1,9 +0,0 @@
-abstract class IObjectModel {
-  late final int id;
-
-  Map<String, dynamic> toMap();
-  List<IObjectModel> queryToList(List<Map<String, dynamic>> query);
-
-  @override
-  String toString();
-}
--- crm_app/lib/main.dart
+++ crm_app/lib/main.dart
@@ -4,99 +4,9 @@ void main() {
   runApp(const MyApp());
 }
 
-class CustomForm extends StatefulWidget {
-  const CustomForm({Key? key}) : super(key: key);
-
-  @override
-  CustomFormState createState() {
-    return CustomFormState();
-  }
-}
-
-// NOTE Important: Call dispose of the TextEditingController when you’ve finished using it. This ensures that you discard any resources used by the object.
-
-// Define a corresponding State class
-// this class holds data related to the form
-class CustomFormState extends State<CustomForm> {
-  // create a global key that uniquely identifies the form Widget
-  // and allows validation of the form
-  //
-  // Note: This is a `GlobalKey<FormState>`,
-  // not a GlobalKey<MyCustomFormState>.
-  final _formKey = GlobalKey<FormState>();
-
-  // Define a text controller and use it to retrieve the current value
-  // of the TextField
-  final controller = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Build a form widget using the _formKey created above
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextFormField(
-              autofocus: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Input Text',
-                hintText: 'Enter Any Value',
-              ),
-              // * The validator receives the text that the user has entered.
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-
-                return null;
-              },
-              controller: controller,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false or otherwise
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        // Retrieve the text that the user has entered by using the
-                        // TextEditingController.
-                        content: Text('Received data: ${controller.text}'),
-                      );
-                    },
-                  );
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -125,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const CustomForm(),
+      body: Container(),
     );
   }
 }
