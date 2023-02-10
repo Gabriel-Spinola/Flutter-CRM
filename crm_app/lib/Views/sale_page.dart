2159354179ff6cd7725d08e7836df2ab56fb5cde
gseven-gdv <zerogravitystd@gmail.com>
2023-02-10 14:05:26 -0300

    add discounts a polish

--- crm_app/lib/Views/sale_page.dart
+++ crm_app/lib/Views/sale_page.dart
@@ -29,16 +29,18 @@ class _SalePageState extends State<SalePage> {
   final GlobalKey<FormState> _form = GlobalKey<FormState>();
 
   final TextEditingController _newQuantityController = TextEditingController();
+  final TextEditingController _discountController = TextEditingController();
   final TextEditingController _changeController = TextEditingController();
 
   final Map<String, double> _pricing = {};
   final List<ProductModel> _products = [];
 
   List<UnitSaleModel> _unitSales = [];
+  ProductModel? data;
+  ProductModel? _dropDownSelectItem;
 
   double _total = 0.0;
 
-  String _keyword = "";
   bool _isLoading = false;
   bool isAdding = false;
 
@@ -91,81 +93,7 @@ class _SalePageState extends State<SalePage> {
               style: Styles.headLine2Style.copyWith(color: Colors.white),
             ),
           ),
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () => showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: const Text("Confirmar"),
-                content: TextField(
-                  keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
-                  ],
-                  autofocus: true,
-                  decoration: const InputDecoration(
-                    hintText: "Valor do cliente",
-                  ),
-                  controller: _changeController,
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: const Text("Sim"),
-                    onPressed: () {
-                      for (var sale in _unitSales) {
-                        context
-                            .read<DatabaseProvider>()
-                            .insert(sale, saleTable);
-
-                        context
-                            .read<DatabaseProvider>()
-                            .delete(sale.id!, unitSaleTable);
-
-                        for (var product in _products) {
-                          context.read<DatabaseProvider>().update(
-                                ProductModel(
-                                  id: product.id,
-                                  productName: product.productName,
-                                  costPrice: product.costPrice,
-                                  sellingPrice: product.sellingPrice,
-                                  amount: product.amount - sale.quantitySold,
-                                ),
-                                productTable,
-                              );
-                        }
-
-                        _products.clear();
-                      }
-
-                      double change =
-                          int.parse(_changeController.text) - _total;
-
-                      _total = 0.0;
-                      _refresh();
-                      Navigator.of(context).pop();
-
-                      showDialog(
-                        context: context,
-                        builder: (context) => Dialog(
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Text("O troco é $change"),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  TextButton(
-                    child: const Text("Não"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
+          confirmSale(),
         ],
       ),
       body: SingleChildScrollView(
@@ -185,7 +113,8 @@ class _SalePageState extends State<SalePage> {
                     refresh: _refresh,
                     sizedBoxWidth: 120,
                     sizedBoxHeight: 120,
-                    listChildrenWidget: _updateQuantity(index),
+                    listChildrenWidget:
+                        _updateQuantityAndDiscountButtons(index),
                   );
                 },
                 shrinkWrap: true,
@@ -197,13 +126,13 @@ class _SalePageState extends State<SalePage> {
     );
   }
 
-  Widget _updateQuantity(int index) {
+  Widget confirmSale() {
     return IconButton(
-      icon: const Icon(Icons.format_list_numbered),
+      icon: const Icon(Icons.save),
       onPressed: () => showDialog(
         context: context,
         builder: (context) => AlertDialog(
-          title: const Text("Quantidade"),
+          title: const Text("Confirmar"),
           content: TextField(
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
             inputFormatters: <TextInputFormatter>[
@@ -211,31 +140,57 @@ class _SalePageState extends State<SalePage> {
             ],
             autofocus: true,
             decoration: const InputDecoration(
-              hintText: "Insira a quantidade",
+              hintText: "Valor do cliente",
             ),
-            controller: _newQuantityController,
+            controller: _changeController,
           ),
           actions: <Widget>[
             TextButton(
-              child: const Text("Submit"),
+              child: const Text("Sim"),
               onPressed: () {
-                int quantity = int.parse(_newQuantityController.text);
+                for (var sale in _unitSales) {
+                  context.read<DatabaseProvider>().insert(sale, saleTable);
 
-                context.read<DatabaseProvider>().update(
-                      UnitSaleModel(
-                        id: _unitSales[index].id,
-                        productName: _unitSales[index].productName,
-                        totalPrice:
-                            _pricing[_unitSales[index].productName]! * quantity,
-                        profit: _unitSales[index].profit * quantity,
-                        quantitySold: quantity,
-                        timeCreated: _unitSales[index].timeCreated,
-                      ),
-                      unitSaleTable,
-                    );
+                  context
+                      .read<DatabaseProvider>()
+                      .delete(sale.id!, unitSaleTable);
 
+                  for (var product in _products) {
+                    context.read<DatabaseProvider>().update(
+                          ProductModel(
+                            id: product.id,
+                            productName: product.productName,
+                            costPrice: product.costPrice,
+                            sellingPrice: product.sellingPrice,
+                            amount: product.amount - sale.quantitySold,
+                          ),
+                          productTable,
+                        );
+                  }
+
+                  _products.clear();
+                }
+
+                double change = int.parse(_changeController.text) - _total;
+
+                _total = 0.0;
                 _refresh();
+                Navigator.of(context).pop();
 
+                showDialog(
+                  context: context,
+                  builder: (context) => Dialog(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text("O troco é $change"),
+                    ),
+                  ),
+                );
+              },
+            ),
+            TextButton(
+              child: const Text("Não"),
+              onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
@@ -245,12 +200,112 @@ class _SalePageState extends State<SalePage> {
     );
   }
 
-  ProductModel? data;
+  Widget _updateQuantityAndDiscountButtons(int index) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.discount),
+          onPressed: () => showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text("Desconto"),
+              content: TextField(
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+                ],
+                autofocus: true,
+                decoration: const InputDecoration(
+                  hintText: "Insira o valor do desconto",
+                ),
+                controller: _discountController,
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("Confirmar"),
+                  onPressed: () {
+                    int discount = int.parse(_discountController.text);
+
+                    context.read<DatabaseProvider>().update(
+                          UnitSaleModel(
+                            id: _unitSales[index].id,
+                            productName: _unitSales[index].productName,
+                            totalPrice:
+                                _pricing[_unitSales[index].productName]! -
+                                    discount,
+                            profit: _unitSales[index].profit - discount,
+                            quantitySold: _unitSales[index].quantitySold,
+                            timeCreated: _unitSales[index].timeCreated,
+                          ),
+                          unitSaleTable,
+                        );
+
+                    _refresh();
+
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.format_list_numbered),
+          onPressed: () => showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text("Quantidade"),
+              content: TextField(
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+                ],
+                autofocus: true,
+                decoration: const InputDecoration(
+                  hintText: "Insira a quantidade",
+                ),
+                controller: _newQuantityController,
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text("Submit"),
+                  onPressed: () {
+                    int quantity = int.parse(_newQuantityController.text);
+
+                    context.read<DatabaseProvider>().update(
+                          UnitSaleModel(
+                            id: _unitSales[index].id,
+                            productName: _unitSales[index].productName,
+                            totalPrice:
+                                _pricing[_unitSales[index].productName]! *
+                                    quantity,
+                            profit: _unitSales[index].profit * quantity,
+                            quantitySold: quantity,
+                            timeCreated: _unitSales[index].timeCreated,
+                          ),
+                          unitSaleTable,
+                        );
+
+                    _refresh();
+
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
   Widget productDropDownViewer() {
     return SizedBox(
       width: AppLayout.getWidth(500.0),
       child: DropdownSearch<ProductModel>(
+        selectedItem: _dropDownSelectItem,
         popupProps: const PopupProps.menu(showSearchBox: true),
         asyncItems: (String filter) => ProductModel.searchModel(
           filter,
@@ -258,7 +313,10 @@ class _SalePageState extends State<SalePage> {
           "${ProductFields.productName} LIKE ?",
         ),
         itemAsString: (ProductModel productModel) => productModel.productName,
-        onChanged: (model) => data = model!,
+        onChanged: (model) {
+          _dropDownSelectItem = model;
+          data = model!;
+        },
         dropdownButtonProps: DropdownButtonProps(
           tooltip: "Strings",
           icon: const Icon(
@@ -328,6 +386,7 @@ class _SalePageState extends State<SalePage> {
   @override
   void dispose() {
     _newQuantityController.dispose();
+    _discountController.dispose();
     _changeController.dispose();
 
     super.dispose();
